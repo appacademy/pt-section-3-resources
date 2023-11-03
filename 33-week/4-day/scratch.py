5e4104110c03fb1a30bf34bc0ed516ca068950bf
will <w.duffy@outlook.com>
2023-11-02 19:28:27 -0500

    w33d4 update

---
+++ 33-week/4-day/scratch.py
@@ -0,0 +1,134 @@
+# print(True == 1)
+# print(True is 1)
+# print(True == "1")
+
+
+# while True:
+#     print("hi")
+#     break;
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+# is_even = lambda num: num % 2 == 0
+
+# print(is_even(4))
+# print(is_even(5))
+
+# y = 200
+# x = 500
+# def make_a_five():
+#     """
+#     This is my function. It doesn't do
+#     anything special. It's just a
+#     function.
+#     """
+#     y = 5
+
+#     """
+#     multi line comment
+
+#     heere
+#     """
+#     return y
+
+
+# help(make_a_five)
+
+
+# print((lambda x, y: y.upper() + " " + x.upper())('Will'))
+
+
+# def adder(x, y):
+#     return x + y
+
+
+# adder = lambda x, y: x + y
+# print(adder(1, 2))
+
+
+# l = [0, 1, 2, 3]
+
+# map_square = map(lambda x: x**2, l)
+
+# print(list(map_square))
+
+
+# def add(a, b, *pizza):
+#     total = a + b;
+#     print('extra pizza', pizza)
+#     for n in pizza:
+#         total += n
+#     return total
+
+# add(2, 3, 4, 5) # Returns 14
+# print(add(2, 3, 4, 5, 6, 7, 8))
+
+
+# def greeting(*args, **kwargs):
+#     # print(isinstance(kwargs, dict))
+
+#     print("kwargs", kwargs)
+#     # for key, val in kwargs.items():
+#     #     print(key, val)
+
+# # greeting("hello", "world")
+# greeting(person="Anthony", person2="Will", person3="Susie" )
+
+
+# print(dinners)
+# print(len(dinners))
+# print(dinners[2])
+# print(dinners[::-1])
+
+# dinners.append("hamburger")
+
+# l = ["hamburger", "taco awesome"]
+# dinners.extend(l)
+# dinners.insert(1, "salmon")
+# print("before: ", dinners)
+
+# dinners = ["pizza", "wings", "steak", "cobb salad", "taco"]
+# dinners.pop()
+# print(dinners)
+# greeting(name="will")
+# numbers = [3, 25, 4, 5, 15]
+numbers = [25, 5, 15]
+
+# print(25 % 5)
+# print(15 % 5)
+# numbers.sort(key=lambda ele: ele % 5)
+new_list = sorted(numbers)
+print(numbers)
+print(new_list)
+
+
+# numbers.sort()
+# print(sum(numbers))
+# print(min(numbers))
+# print(max(numbers))
+
+
+# print(dinners[::-1])
+# # dinners.append("turkey")
+# print(dinners)
+# # dinners.extend(["sloppy joe", "corn"])
+# print(dinners)
+# dinners.insert(0, "shrimp")
+# print(dinners)
+# dinners.remove("taco")
+# print(dinners)
+# dinners.pop(1)
+# print(dinners)
+# # dinners.sort()
+# # print(dinners)
+
+# dinners.sort(key=lambda ele: ele[1])
+
+
+# # def hello_world(test, name, next_param="will", *args, **kwargs):
+# #     print(test, name, next_param, args, kwargs)
+
+# # hello_world("hi", "hello", "test", "Test", 1, 2, hello = "world")
