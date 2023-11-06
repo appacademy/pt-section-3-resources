654803861042a75c320b7155616b7e114bb149c2
will <w.duffy@outlook.com>
2023-11-06 17:28:25 -0600

    added w34d1 notes

--- 34-week/1-day/scratch.py
+++ 34-week/1-day/scratch.py
@@ -1,3 +1,50 @@
 test = ["item", [], []]
 print(any(test)) # True
 print(all(test)) # False
+
+
+phones = [
+    {
+        "brand": "Apple",
+        "model": "iPhone 13 Pro",
+        "cost": 929,
+        "color": "alpine green"
+    },
+    {
+        "brand": "Samsung",
+        "model": "Galaxy S22+",
+        "cost": 999,
+        "color": "black"
+    },
+    {
+        "brand": "Google",
+        "model": "Pixel 6",
+        "cost": 599,
+        "color": "kinda coral"
+    },
+    {
+        "brand": "Apple",
+        "model": "iPhone 13 Pro",
+        "cost": 929,
+        "color": "gold"
+    },
+    {
+        "brand": "Google",
+        "model": "Pixel 6",
+        "cost": 599,
+        "color": "stormy black"
+    }
+]
+
+# Write your code here.
+
+def get_unique_models(phone_list):
+    all_models = [brand for brand in phone_list]
+    print(all_models)
+
+def map_to_names(phone_list):
+    # map_obj = map(
+
+unique_models = list(get_unique_models(phones))
+print(unique_models)                # iPhone 13 Pro, Galaxy S22+, Pixel 6 (dictionaries)
+print(map_to_names(unique_models))  # iPhone 13 Pro, Galaxy S22+, Pixel 6
