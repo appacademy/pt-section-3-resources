f7747e15174e46add9cd514b71ccfcd83945df68
Jesse Brooks <brooks.jesse83@gmail.com>
2023-11-28 20:26:43 -0800

    added pst notes

---
+++ 33-week/2-day/scratch_pst.py
@@ -0,0 +1,140 @@
+# correct indentation
+# if True:
+#     print('Hello world')
+
+# incorrect indentation
+# if True:
+#     pass
+# print("Hello world")
+
+# returning None from a function (because we didn't use return)
+# defining a function
+# def print_hello(name):
+#     print("Hello", name)
+
+# print_hello("Jesse")
+
+# returning stuff from a function
+# def print_hello(name):
+#     return "Hello " + name
+
+# print(print_hello("Jesse"))
+
+# snake_case
+# every_word_starts_undercase_and_is_separated_by_an_underscore = "Hello"
+
+# truthy = True
+
+# falsey = False
+
+# print(truthy == falsey)
+# print(truthy < falsey)
+
+# tester = None
+
+# if tester:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if True and False:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if True or False:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if not False:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if True and 10/0:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if True or 10/0:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# num = 11
+
+# if num % 2 == 0:
+#     print(f"2 is a factor of {num}")
+# elif num % 5 == 0:
+#     print(f"5 is a factor of {num}")
+# else:
+#     print(f"2 and 5 are not factors of {num}")
+
+# if num % 2 == 0: print(f"2 is a factor of {num}")
+# elif num % 5 == 0: print(f"5 is a factor of {num}")
+# else: print(f"2 and 5 are not factors of {num}")
+
+# if (condition):
+# if condition:
+
+# string = 'hello '
+# another_string = "April 2023 cohort"
+# multi_string = """this is a multi-line string"""
+
+# print(string)
+# print(another_string)
+# print(multi_string)
+
+# string concatenation
+# print(string, another_string)
+# print(string + another_string)
+
+# formatted_string = f"my formated string: {string} {another_string}"
+# print(formatted_string)
+
+# math_string = f"the answer to 5 + 10 is: {5 + 10}"
+# print(math_string)
+
+# y = "y"
+# e = "e"
+# a = "aaaaa"
+# h = "h"
+
+# print(y + e + a*20 + h)
+
+# string = "The sky is blue today!"
+
+# print(string[len(string) - 1])
+# print(string([0]))
+# print(string[11])
+# print(string[-10])
+# print(string[0:1])
+# print(string[0:len(string)])
+
+# print(string.index("skyz"))
+# print(string.count("sky"))
+
+# print(string.upper())
+# print(string.lower())
+# print(string.title())
+
+num_a = 21
+
+num_b = 10
+
+# print(num_a + num_b)
+# print(num_a - num_b)
+# print(num_a * num_b)
+# print(num_a / num_b)
+# print(num_a // num_b)
+# print(num_a ** num_b)
+# print(num_a % num_b)
+
+print(num_a)
+num_a += 1  # num_a = num_a + 1
+print(num_a)
+num_a *= 3
+print(num_a)
+num_a /= 2
+print(num_a)
