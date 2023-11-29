7bf2c95138c39ac4eabf369e62e011c263edc5af
Jesse Brooks <brooks.jesse83@gmail.com>
2023-11-28 16:24:38 -0800

    w33d2 notes

---
+++ 33-week/2-day/scratch.py
@@ -0,0 +1,144 @@
+# right indentation
+# if True:
+#     print("Hello World")
+
+# wrong indentation
+# if True:
+# print("Hello World")
+
+# one line function definition
+# def print_hello(): print("Hello")
+
+# multi-line (normal) funtion definition
+# def print_hello():
+#     print("Hello")
+
+# print_hello()
+
+# def add_nums(a, b):
+#     return a + b
+
+# def add_nums(a,b):
+#     a + b
+
+# print(add_nums(1, 5))
+
+# in_class = True
+# in_class = False
+
+# print(in_class < True)
+
+# truthy/falsey values
+# value = {}
+
+# if value == True:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if 0 and True:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if 0 or True:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if not 0:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if 0 or 1:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if True and 10/0:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if 0 and 10/0:
+#     print("Truthy")
+# else:
+#     print("Falsey")
+
+# if True:
+#     print("Truthy")
+#     if 10/0:
+#         print("Falsey")
+
+# x = 10/0
+
+# num = 11
+
+# if num % 2 == 0:
+#     print("2 is a factor")
+# elif num % 5 == 0:
+#     print("5 is a factor")
+# else:
+#     print("no factors found")
+
+# my_name = 'Jesse's food'
+# my_name = "Jesse"
+
+# my_name = f"{my_name} likes hiking"
+
+# string = f"The result of 5 + 5 is: {5 + 5}"
+
+# string = """My name is Jesse
+# I like to hike"""
+
+# # print(my_name)
+# print(string)
+
+# y = "y"
+# e = "e"
+# a = "aaaaa"
+# h = "h"
+
+# print(y + e + a*10 + h)
+
+# string = "Jesse"
+
+# print(string[0:4])
+# print(string[0:3])
+
+# string = "pizza pizza"
+
+# print(string.index("z"))
+
+# print(string.count("pizza"))
+
+# print(string.split("p"))
+
+# print(string.join("pizza"))
+
+# print(string.upper())
+# print(string.lower())
+# print(string.title())
+
+SOME_NUM = 5
+
+SOME_NUM = 3
+
+num = 2
+
+# print(SOME_NUM + num)
+# print(SOME_NUM - num)
+# print(SOME_NUM * num)
+# print(SOME_NUM / num)
+# print(SOME_NUM // num)
+# print(SOME_NUM ** num)
+# print(SOME_NUM % num)
+
+new_num = 5
+
+print(new_num)
+new_num += 1 # new_num = new_num + 1
+print(new_num)
+new_num *= 3
+print(new_num)
