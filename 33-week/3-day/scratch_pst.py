a8316d6364397abc00e492255e0a573c503d7a2f
Jesse Brooks <brooks.jesse83@gmail.com>
2023-11-29 20:01:27 -0800

    lecture notes pst

---
+++ 33-week/3-day/scratch_pst.py
@@ -0,0 +1,128 @@
+# ODD_NUM = 5
+
+# ODD_NUM = "hello"
+
+# print(ODD_NUM)
+
+# num = num_2 = num_3 = 10
+
+# print(num)
+# print(num_2)
+# print(num_3)
+
+# num = "5"
+# num = int(5)
+
+# print(isinstance(num, int))
+
+# print(num)
+
+# float_num = 1.1
+# float_num = float(1.1)
+
+# print(float_num)
+
+# hello_string = "hello"
+# hello_string = 'hello'
+# hello_string = """hello"""
+# hello_string = str("hello")
+# hello_string = f"5 + 5 = {5 + 5}"
+
+# print(hello_string)
+
+# a = '5'
+
+# print(isinstance(int(a), int))
+
+# nums = [1,2,3,4,5]
+# nums = list([1,2,3,4,5])
+
+# print(nums[3])
+# nums[3] = 12
+# print(nums[3])
+
+# state_capitals = {"California": "Sacramento", "Arizona": "Phoenix"}
+
+# b = state_capitals
+
+# b["California"] = "something else"
+
+# print(state_capitals)
+
+# state_capitals = dict({"California": "Sacramento", "Arizona": "Phoenix"})
+
+# print(state_capitals.California)
+# print(state_capitals["California"])
+# print(state_capitals.get("California"))
+
+# odd_nums = (1,3,5,7,9)
+# odd_nums = tuple((1,3,5,7,9))
+# odd_nums[0] = 5
+
+# print(odd_nums)
+# print(odd_nums[0])
+
+# even_nums = set({2,4,6,8,10})
+# even_nums = {2,4,6,8,10,10,10}
+# print(even_nums)
+# even_nums.add(12)
+# print(even_nums)
+
+# print({} == {})
+
+# list_1 = [1,2,3]
+# list_2 = [1,2,3]
+
+# list_1_ref = list_1
+
+# print(id(list_1))
+# print(id(list_2))
+# print(id(list_1_ref))
+
+# print(list_1 is list_2)
+
+# nums = None
+
+# if nums is None:
+#     print("it is none")
+# else:
+#     print("its not none")
+
+# is_daytime = 0
+# print(id(0))
+# print(id(is_daytime))
+# print(id(False))
+
+# print(is_daytime is False)
+
+# while condition:
+# nums = [1,2,3,4,5]
+# index = 0
+# print(len(nums))
+# while (index < len(nums)):
+# # while index < len(nums):
+#     print(nums[index])
+#     index += 1
+#     # index = index + 1
+
+# for element in iterable:
+# for num in nums:
+#     print(num)
+
+# print(1 in nums)
+
+# nums = [1,3,5,7,9,12,14]
+
+# num = 5
+# divide_num = 0
+
+# print(num / divide_num)
+# print("division successful")
+# try:
+#     num / divide_num
+# except ZeroDivisionError:
+#     print("can't divide by 0, please choose another number")
+# else:
+#     print('hello from else')
+# finally:
+#     print("im always here")
