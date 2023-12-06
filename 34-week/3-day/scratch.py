292357149b320e6883ae21c077b10086821aba59
jesse <brooks.jesse83@gmail.com>
2023-12-06 15:03:27 -0800

    notes

---
+++ 34-week/3-day/scratch.py
@@ -0,0 +1,21 @@
+# Classes
+
+# Creating a class
+
+    # class constructor
+        # class instance variables
+    # instance methods
+    
+    # class methods
+
+    # static methods
+
+    # getters
+
+    # setters
+    
+# decorators
+
+# imports
+
+
